827edf1c4f40ae0903962bbfd50758be7b354780
kjh <u9330029@gmail.com>
2022-03-15 09:33:09 +0900

    0314

---
+++ js_day6/4_string메소드.js
@@ -0,0 +1,49 @@
+let txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = txt.length;
+console.log(length)
+
+let str = "Apple, Banana, Kiwi";
+let part = str.slice(7, 13);
+console.log(part)
+part = str.slice(-12, -6);
+console.log(part)
+str = "Apple, Banana, Kiwi";
+part = str.substring(7, 13);
+console.log(part)
+str = "Apple, Banana, Kiwi";
+part = str.substr(7);
+console.log(part)
+str = "Apple, Banana, Kiwi";
+part = str.substr(-4);
+console.log(part)
+text = "Please visit Microsoft!";
+newText = text.replace("Microsoft", "W3Schools");
+console.log(newText)
+text = "Please visit Microsoft and Microsoft!";
+newText = text.replace(/Microsoft/g, "W3Schools");
+console.log(newText)
+let text1 = "Hello World!";
+let text2 = text1.toUpperCase();
+console.log(text2)
+text1 = "Hello";
+text2 = "World";
+let text3 = text1.concat(" ", text2);
+console.log(text3)
+text1 = "      Hello World!      ";
+text2 = text1.trim();
+console.log(text2)
+ text = "5";
+let padded = text.padStart(4,0);
+console.log(padded)
+text = "HELLO WORLD";
+char = text.charAt(0);
+console.log(char)
+
+
+
+
+
+
+
+
+
